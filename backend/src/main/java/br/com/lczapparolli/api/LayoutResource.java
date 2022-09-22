56aba64d0327f08cd53eeee92133fed8499a8e68
Luis Carlos Zapparolli Neto <lczapparolli@gmail.com>
2022-09-22 00:49:09 -0300

    Adicionando documentação ao endpoint

--- backend/src/main/java/br/com/lczapparolli/api/LayoutResource.java
+++ backend/src/main/java/br/com/lczapparolli/api/LayoutResource.java
@@ -1,6 +1,7 @@
 package br.com.lczapparolli.api;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.eclipse.microprofile.openapi.annotations.enums.SchemaType.ARRAY;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -9,9 +10,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import br.com.lczapparolli.dto.ErroDTO;
+import br.com.lczapparolli.dto.LayoutDTO;
 import br.com.lczapparolli.mapper.LayoutMapper;
 import br.com.lczapparolli.mapper.ResponseMapper;
 import br.com.lczapparolli.service.LayoutService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 /**
  * Endpoints para manipulação dos layouts
@@ -20,6 +29,7 @@ import br.com.lczapparolli.service.LayoutService;
  */
 @Path("/layout")
 @Produces(APPLICATION_JSON)
+@Tag(name = "Layout", description = "Endpoints para a manipulação dos layouts")
 public class LayoutResource {
 
     @Inject
@@ -32,7 +42,14 @@ public class LayoutResource {
      * @return Retorna a resposta HTTP com os layouts encontrados
      */
     @GET
-    public Response listarLayouts(@QueryParam("ativo") Boolean ativo) {
+    @Operation(description = "Lista os layouts cadastrados")
+    @APIResponse(responseCode = "200", name = "Sucesso", description = "Foi possível obter a lista de layouts", content = @Content(schema = @Schema(type = ARRAY, implementation = LayoutDTO.class)))
+    @APIResponse(responseCode = "400", name = "Dados inválidos", description = "Os dados informados na requisição não são válidos", content = @Content(schema = @Schema(type = ARRAY, implementation = ErroDTO.class)))
+    @APIResponse(responseCode = "500", name = "Erro interno", description = "Ocorreu um erro internamente que impediu o processamento da solicitação", content = @Content(schema = @Schema(type = ARRAY, implementation = ErroDTO.class)))
+    public Response listarLayouts(
+            @Parameter(description = "Indica se devem ser retornados apenas os layouts ativos ou inativos. Se não informado, todos os layouts são retornados", example = "true")
+            @QueryParam("ativo")
+            Boolean ativo) {
         var resultado = layoutService.listarLayouts(ativo);
         return ResponseMapper.construirRespostaLista(resultado, LayoutMapper::toDTO);
     }
--- backend/src/main/java/br/com/lczapparolli/dto/LayoutDTO.java
+++ backend/src/main/java/br/com/lczapparolli/dto/LayoutDTO.java
@@ -2,27 +2,34 @@ package br.com.lczapparolli.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 /**
  * Dados do layout a serem expostos
  */
 @Data
+@EqualsAndHashCode
 @Builder
+@Schema(name = "Layout", description = "Dados do layout para importação")
 public class LayoutDTO {
 
     /**
-     * Identificação do layout
+     * Identificação única do Layout
      */
+    @Schema(name = "id", description = "Identificação única do Layout", example = "1", required = true)
     private Integer id;
 
     /**
      * Descrição legível
      */
+    @Schema(name = "descricao", description = "Descrição legível", example = "Layout de testes", required = true)
     private String descricao;
 
     /**
      * Identificação se o layout está ativo ou não
      */
+    @Schema(name = "ativo", description = "Identificação se o layout está tivo ou não", example = "true", required = true)
     private Boolean ativo;
 
 }
