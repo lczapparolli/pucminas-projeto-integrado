7aeaae78eccc69d4cb0d99410becf03ce42ab535
Luis Carlos Zapparolli Neto <lczapparolli@gmail.com>
2022-09-22 00:08:43 -0300

    Criando métodos para geração das respostas HTTP

--- backend/src/main/java/br/com/lczapparolli/api/ImportacaoResource.java
+++ backend/src/main/java/br/com/lczapparolli/api/ImportacaoResource.java
@@ -1,10 +1,9 @@
 package br.com.lczapparolli.api;
 
+import static br.com.lczapparolli.mapper.ResponseMapper.construirResposta;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import static javax.ws.rs.core.MediaType.MULTIPART_FORM_DATA;
-import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
-import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -13,9 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import br.com.lczapparolli.dto.ErroDTO;
 import br.com.lczapparolli.dto.ImportacaoNovoDTO;
-import br.com.lczapparolli.erro.ResultadoOperacao;
 import br.com.lczapparolli.service.ImportacaoService;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
@@ -47,24 +44,6 @@ public class ImportacaoResource {
     @RequestBody(name = "DadosNovaImportacao", description = "Conteúdo da requisição para iniciar a importação")
     public Response iniciarImportacao(@MultipartForm ImportacaoNovoDTO importacaoNovoDTO) {
         var resultado = importacaoService.iniciarImportacao(importacaoNovoDTO);
-        if (resultado.possuiErros()) {
-            return construirRespostaErro(resultado);
-        }
-
-        return Response.noContent().build();
-    }
-
-    /**
-     * Constroi uma resposta de erro, identificando o código HTTP que deve ser retornado
-     *
-     * @param resultadoOperacao Objeto contendo os erros retornados
-     * @return Retorna o objeto da resposta construído
-     */
-    private Response construirRespostaErro(ResultadoOperacao<?> resultadoOperacao) {
-        var possuiErroValidacao = resultadoOperacao.getErros().stream().anyMatch(ErroDTO::isValidacao);
-        return Response
-                .status(possuiErroValidacao ? BAD_REQUEST : INTERNAL_SERVER_ERROR)
-                .entity(resultadoOperacao.getErros())
-                .build();
+        return construirResposta(resultado);
     }
 }
