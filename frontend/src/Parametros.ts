521d877be59b1203329afd2a59e0434a3a494ae8
Luis Carlos Zapparolli Neto <lczapparolli@gmail.com>
2022-10-07 21:29:50 -0300

    Parametrizando propriedades no frontend

--- frontend/src/Parametros.ts
+++ frontend/src/Parametros.ts
@@ -1,14 +1,14 @@
 /**
  * Base da URL para execução dos serviços
  */
-const BASE_URL = "http://localhost:8080";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 /**
  * URL completa para o consumo dos endpoints de layout
  */
-export const LAYOUT_URL = `${BASE_URL}/layout`;
+export const LAYOUT_URL = `${API_BASE_URL}/layout`;
 
 /**
  * URL completa para o consumo dos endpoints de importação
  */
-export const IMPORTACAO_URL = `${BASE_URL}/importacao`;
+export const IMPORTACAO_URL = `${API_BASE_URL}/importacao`;
